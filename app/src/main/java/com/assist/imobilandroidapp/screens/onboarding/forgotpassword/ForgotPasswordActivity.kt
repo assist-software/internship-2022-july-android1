6131557a0192afb7c5ad62a6354c061aa26520cd
PadurariuLiviu <liviu.padurariu@outlook.com>
2022-07-15 10:57:33 +0300

    Log in and Forgot Password Validation update

--- app/src/main/java/com/assist/imobilandroidapp/screens/onboarding/forgotpassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/assist/imobilandroidapp/screens/onboarding/forgotpassword/ForgotPasswordActivity.kt
@@ -10,72 +10,59 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.assist.imobilandroidapp.R
+import com.assist.imobilandroidapp.databinding.ActivityForgotPasswordBinding
+import com.assist.imobilandroidapp.databinding.ActivityLogInBinding
 import com.assist.imobilandroidapp.screens.onboarding.login.LogInActivity
+import com.assist.imobilandroidapp.validator.Validator
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
     private lateinit var emailEditText: EditText
     private lateinit var emailErrorMsg: TextView
     private lateinit var sendReqBtn: Button
+    private lateinit var binding: ActivityForgotPasswordBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
-        innitButtons()
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initButtons()
         sendReqForgotPass()
     }
 
-    private fun innitButtons() {
-        val sendResetLinkButton = findViewById<Button>(R.id.SendResetLinkButton)
-        val backToLoginButton = findViewById<TextView>(R.id.BackToLogInButton)
-        sendResetLinkButton.setOnClickListener {
-            Toast.makeText(this, "Send Reset Link Button", Toast.LENGTH_SHORT).show()
+    private fun initButtons() {
+        binding.btnSendReq.setOnClickListener {
+            Toast.makeText(this, getString(R.string.SendResetLink), Toast.LENGTH_SHORT).show()
         }
-        backToLoginButton.setOnClickListener {
+        binding.tvBackLogin.setOnClickListener {
             val intent = Intent(this@ForgotPasswordActivity, LogInActivity::class.java)
             this.startActivity(intent)
         }
     }
 
-    private fun validateEmail(): Boolean {
-        emailEditText = findViewById(R.id.EditTextEmail)
-        emailErrorMsg = findViewById(R.id.ErrorMsgEmail)
-
-        val padding = emailEditText.paddingLeft
-        emailEditText.setPadding(padding, padding, padding, padding)
-        val email = emailEditText.text.toString()
-
-        if (email.isEmpty()) {
-            emailErrorMsg.setText(R.string.empty_field)
-            emailErrorMsg.isVisible = true
-            emailEditText.background =
-                ContextCompat.getDrawable(applicationContext, R.drawable.input_border_red)
-            emailEditText.setPadding(padding, padding, padding, padding)
-            return false
-        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            emailErrorMsg.setText(R.string.wrong_email)
-            emailErrorMsg.isVisible = true
-            emailEditText.background =
-                ContextCompat.getDrawable(applicationContext, R.drawable.input_border_red)
-            emailEditText.setPadding(padding, padding, padding, padding)
-            return false
-        } else {
-            emailErrorMsg.isVisible = false
-            emailEditText.background =
-                ContextCompat.getDrawable(applicationContext, R.drawable.loginbox_border)
-            emailEditText.setPadding(padding, padding, padding, padding)
-            return true
-        }
-    }
-
     private fun sendReqForgotPass() {
-        sendReqBtn = findViewById(R.id.SendResetLinkButton)
-        sendReqBtn.setOnClickListener {
-            if (!validateEmail()) {
-                Toast.makeText(applicationContext, "Invalid email", Toast.LENGTH_LONG)
+        sendReqBtn = findViewById(R.id.btn_send_req)
+        binding.btnSendReq.setOnClickListener {
+            if (!Validator(
+                    binding.etEmailInput,
+                    binding.tvErrorEmail,
+                    this,
+                    resources
+                ).validateEmail()
+            ) {
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.wrong_email),
+                    Toast.LENGTH_LONG
+                )
                     .show()
             } else {
-                Toast.makeText(applicationContext, "Cool, good job", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.correct_email),
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
