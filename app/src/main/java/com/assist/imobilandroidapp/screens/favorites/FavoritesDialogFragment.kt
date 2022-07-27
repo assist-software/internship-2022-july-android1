cece919a6bb05d252faf1ed90afa6c5ccb4ca1d1
PadurariuLiviu <liviu.padurariu@outlook.com>
2022-07-27 16:55:20 +0300

    last update

--- app/src/main/java/com/assist/imobilandroidapp/screens/favorites/FavoritesDialogFragment.kt
+++ app/src/main/java/com/assist/imobilandroidapp/screens/favorites/FavoritesDialogFragment.kt
@@ -3,10 +3,6 @@ package com.assist.imobilandroidapp.screens.favorites
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.assist.imobilandroidapp.R
 
--- app/src/main/java/com/assist/imobilandroidapp/screens/search/SearchActivity.kt
+++ app/src/main/java/com/assist/imobilandroidapp/screens/search/SearchActivity.kt
@@ -1,5 +1,4 @@
 package com.assist.imobilandroidapp.screens.search
-
 import android.content.Intent
 import android.os.Bundle
 import android.widget.SearchView
@@ -21,50 +20,32 @@ import com.assist.imobilandroidapp.screens.favorites.FavoritesDialogFragment
 import com.assist.imobilandroidapp.screens.listing.ListingScreenActivity
 import com.assist.imobilandroidapp.screens.profile.MainProfileActivity
 import com.assist.imobilandroidapp.screens.profile.MessagesActivity
+import com.assist.imobilandroidapp.storage.SharedPrefManager
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-
 class SearchActivity : AppCompatActivity(), ListingItemWithDescAdapter.OnFavIconClick {
-
     private lateinit var binding: ActivitySearchBinding
     private var searchQuery: String? = ""
     private var userType: Int? = 0
-
     val foundListings: ArrayList<ListingFromDBObject> = arrayListOf()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val bundle = intent.extras
         searchQuery = bundle?.getString("searchQuery")
         userType = bundle?.getInt("userType")
-
         getListings()
         editSearchText()
         onProfileIconClick()
         onSearchIconClick()
-        onMessagesBtnClick()
+        onMessageClick()
+        onToolbarFavIconClick()
     }
-
-    private fun onMessagesBtnClick() {
-        binding.fab.setOnClickListener {
-            if(userType == StartFragment.UserTypeConstants.LOGGED_IN_USER) {
-                intent = Intent(this, MessagesActivity::class.java)
-                startActivity(intent)
-            }
-            else {
-                LoginDialogFragment().show(supportFragmentManager, LoginDialogFragment.TAG)
-            }
-        }
-    }
-
     private fun editSearchText() {
         var newText = ""
-        newText = if(foundListings.size != 0) {
+        newText = if (foundListings.size != 0) {
             foundListings.size.toString() + " " +
                     getText(R.string.search_results_placeholder_activity).toString() + " " + searchQuery
         } else {
@@ -72,16 +53,13 @@ class SearchActivity : AppCompatActivity(), ListingItemWithDescAdapter.OnFavIcon
         }
         binding.tvTitle.text = newText
     }
-
     private fun initRV() {
         val listingsRecyclerView: RecyclerView = binding.rvListings
         val listingItemsAdapter = ListingItemWithDescAdapter(foundListings, this)
-
         listingsRecyclerView.adapter = listingItemsAdapter
         listingsRecyclerView.layoutManager = LinearLayoutManager(applicationContext)
         userType?.let { listingItemsAdapter.setUserType(it) }
     }
-
     private fun getListings() {
         searchQuery.let { query ->
             if (query != null) {
@@ -99,7 +77,6 @@ class SearchActivity : AppCompatActivity(), ListingItemWithDescAdapter.OnFavIcon
                                         Toast.LENGTH_LONG
                                     ).show()
                                 }
-
                                 200 -> {
                                     if (response.body()?.isNotEmpty() == true) {
                                         foundListings.addAll(response.body()!!)
@@ -115,7 +92,6 @@ class SearchActivity : AppCompatActivity(), ListingItemWithDescAdapter.OnFavIcon
                                 }
                             }
                         }
-
                         override fun onFailure(
                             call: Call<List<ListingFromDBObject>>,
                             t: Throwable
@@ -130,36 +106,70 @@ class SearchActivity : AppCompatActivity(), ListingItemWithDescAdapter.OnFavIcon
             }
         }
     }
-
     override fun onFavIconClick(listingItemWithDesc: ListingFromDBObject) {
-        binding.toolbar.ivFavouritesIcon.setOnClickListener {
-            if(userType == StartFragment.UserTypeConstants.LOGGED_IN_USER) {
-                intent = Intent(this, FavoritesActivity::class.java)
-                startActivity(intent)
-            }
-            else {
-                FavoritesDialogFragment().show(supportFragmentManager, LoginDialogFragment.TAG)
+        if (userType == StartFragment.UserTypeConstants.LOGGED_IN_USER) {
+            SharedPrefManager.getInstance().fetchUserId()?.let {
+                RetrofitClient.instance.addToFavoritesList(it, listingItemWithDesc.id)
+                    .enqueue(object : Callback<String> {
+                        override fun onResponse(
+                            call: Call<String>,
+                            response: Response<String>
+                        ) {
+                            when (response.code()) {
+                                400, 401 -> {
+                                    Toast.makeText(
+                                        applicationContext,
+                                        getText(R.string.something_wrong).toString(),
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                                200 -> {
+                                    if (response.body()?.isNotEmpty() == true) {
+                                        Toast.makeText(
+                                            applicationContext,
+                                            getText(R.string.added_to_fav).toString(),
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                    } else {
+                                        Toast.makeText(
+                                            applicationContext,
+                                            getText(R.string.nothing_found).toString(),
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                    }
+                                }
+                            }
+                        }
+                        override fun onFailure(call: Call<String>, t: Throwable) {
+                            Toast.makeText(
+                                applicationContext,
+                                t.message,
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    })
             }
         }
+        else {
+            FavoritesDialogFragment().show(supportFragmentManager, LoginDialogFragment.TAG)
+        }
     }
-
     override fun onListingClick(ListingItemWithDesc: ListingFromDBObject) {
         val intent = Intent(this, ListingScreenActivity::class.java)
+        intent.putExtra("userType", userType)
+        intent.putExtra("id", ListingItemWithDesc.id)
         startActivity(intent)
     }
-
     private fun onProfileIconClick() {
         binding.toolbar.ivProfilePic.setOnClickListener {
-           if(userType == StartFragment.UserTypeConstants.LOGGED_IN_USER) {
-               intent = Intent(this, MainProfileActivity::class.java)
-               startActivity(intent)
-           }
-            else {
-               LoginDialogFragment().show(supportFragmentManager, LoginDialogFragment.TAG)
-           }
+            if (userType == StartFragment.UserTypeConstants.LOGGED_IN_USER) {
+                intent = Intent(this, MainProfileActivity::class.java)
+                startActivity(intent)
+            } else {
+                LoginDialogFragment().show(supportFragmentManager, LoginDialogFragment.TAG)
+            }
         }
     }
-
     private fun onSearchIconClick() {
         binding.toolbar.ivSearchIcon.setOnClickListener {
             binding.svSearch.isVisible = true
@@ -167,7 +177,6 @@ class SearchActivity : AppCompatActivity(), ListingItemWithDescAdapter.OnFavIcon
                 override fun onQueryTextChange(p0: String?): Boolean {
                     return false
                 }
-
                 override fun onQueryTextSubmit(text: String): Boolean {
                     searchQuery = text
                     binding.svSearch.isGone = true
@@ -178,6 +187,23 @@ class SearchActivity : AppCompatActivity(), ListingItemWithDescAdapter.OnFavIcon
                     return false
                 }
             })
+        }
+    }
+    private fun onMessageClick() {
+        binding.fab.setOnClickListener {
+            val intent = Intent(applicationContext, MessagesActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun onToolbarFavIconClick() {
+        binding.toolbar.ivFavouritesIcon.setOnClickListener {
+            if(userType == StartFragment.UserTypeConstants.LOGGED_IN_USER) {
+                val intent = Intent(applicationContext, FavoritesActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                FavoritesDialogFragment().show(supportFragmentManager, LoginDialogFragment.TAG)
+            }
         }
     }
 }
